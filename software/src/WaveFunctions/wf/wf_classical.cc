49077ee2bff9f2b4b719511f0cf7ca7a761778c9
scottedwardpratt <prattsc@msu.edu>
2021-11-06 10:09:04 -0400

    fixed

--- software/src/WaveFunctions/wf/wf_classical.cc
+++ software/src/WaveFunctions/wf/wf_classical.cc
@@ -5,23 +5,36 @@ CWaveFunction_classical::CWaveFunction_classical(){
 }
 
 double CWaveFunction_classical::CalcPsiSquared(double q,double r,double ctheta,double m1,double m2,int q1q2){
-	double mu,psisquared=0.0,eratio,root,sign;
-	mu=m1*m2/(m1+m2);
-	eratio=2.0*mu*q1q2*ALPHA*HBARC/(r*q*q);  // ratio of PE to E
-	if(eratio<1.0){
-		if(fabs(eratio)<1.0E-7){
-			psisquared=1.0;
+	double psisquared=1.0;
+	if(r>1000.0){
+		psisquared=1.0;
+	}
+	else{
+		double mu,eratio,root,sign,stheta,stheta0,ctheta0,Jacobian;
+		mu=m1*m2/(m1+m2);
+		eratio=2.0*mu*q1q2*ALPHA*HBARC/(r*q*q);  // ratio of PE to E
+		//printf("mu=%g, eratio=%g, q=%g, r=%g, q1q2=%d, ALPHA=%g, HBARC=%g\n",mu,eratio,q,r,q1q2,ALPHA,HBARC);
+		root=1.0-2.0*eratio/(1.0+ctheta);
+		if(root>0.0){
+			root=sqrt(root);
+			stheta=sqrt(1.0-ctheta*ctheta);
+			sign=1.0;
+			stheta0=0.5*stheta*(1.0+sign*root);
+			ctheta0=sqrt(1.0-stheta0*stheta0);
+			Jacobian=(0.5/ctheta0)*(ctheta+sign*(ctheta-eratio)/root);
+			Jacobian*=0.5*(1.0+sign*root);	
+			psisquared=fabs(Jacobian);
+			sign=-1.0;
+			stheta0=0.5*stheta*(1.0+sign*root);
+			ctheta0=sqrt(1.0-stheta0*stheta0);
+			Jacobian=(0.5/ctheta0)*(ctheta+sign*(ctheta-eratio)/root);
+			Jacobian*=0.5*(1.0+sign*root);
+			psisquared+=fabs(Jacobian);
+			psisquared*=sqrt(1.0-eratio);
+			//printf("psisquared=%g\n",psisquared);
 		}
-		else{
-			root=1.0-2.0*eratio/(1.0+ctheta);
-			if(root>0.0){
-				root=sqrt(root);
-				sign=1.0;
-				psisquared =fabs(1.0+sign*eratio*eratio/((1.0+sign*root)*(1.0+sign*root)*root*(1.0+ctheta)*(1.0+ctheta)));
-				sign=-1.0;
-				psisquared+=fabs(1.0+sign*eratio*eratio/((1.0+sign*root)*(1.0+sign*root)*root*(1.0+ctheta)*(1.0+ctheta)));
-			}
-		}
+		else
+			psisquared=0.0;
 	}
 	return psisquared;
 }
