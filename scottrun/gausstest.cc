8737481bb3b6e3e3077b94c3cb22015bc3776915
scottedwardpratt <prattsc@msu.edu>
2022-09-28 09:38:12 +0200

    dummy message

--- scottrun/gausstest.cc
+++ scottrun/gausstest.cc
@@ -1,4 +1,4 @@
-#include "coral.h"
+#include "msu_coral/coral.h"
 #include <vector>
 using namespace std;
 
@@ -9,53 +9,54 @@ int main(){
   int iq,Nq,iqdir;
   vector<vector<double>> cf;
   int imc,NMC;
-  CRandy randy(-time(NULL));
-	string wf_parsfilename="parameters/wfparameters.dat";
-  wf=new CWaveFunction_pp_schrod(wf_parsfilename);
-	CparameterMap parmap;
-	string parsfilename="coralpars.dat";
-	parmap.ReadParsFromFile(parsfilename);
-	NMC=parmap.getD("NMC",10000);
-	Rz=parmap.getD("Rlong",4.0);
-	Ry=parmap.getD("Rside",3.0);
-	Rx=parmap.getD("Rout",3.5);
-	offset=parmap.getD("OFFSET",0.0);  // separation between two particles (along outward direction)
+  Crandy randy(-time(NULL));  // random number generator (in msu_commonutils)
+  
+  wf=new CWaveFunction_pp_schrod("parameters/wfparameters.dat"); // This is for pp, solves Schrod. eq. 
+  // parameter file includes information for solving and storing wave functions
+  
+  CparameterMap parmap;  
+  parmap.ReadParsFromFile("parameters/coralpars.dat");  // Reads
+  NMC=parmap.getD("NMC",10000);  // Number of Monte Carlo points to sample CF
+  Rz=parmap.getD("Rlong",4.0);
+  Ry=parmap.getD("Rside",3.0);
+  Rx=parmap.getD("Rout",3.5);
+  offset=parmap.getD("OFFSET",0.0);  // separation between two particles (along outward direction)
 
-	Nq=wf->GetNQMAX();
-	cf.resize(3);
-	for(iqdir=0;iqdir<3;iqdir++){
-		cf[iqdir].resize(Nq);
-		for(iq=0;iq<Nq;iq++){
-			q=wf->GetQ(iq);
-			if(iqdir==0){
-				qx=q; qy=qz=0.0;
-			}
-			else if(iqdir==1){
-				qy=q; qx=qz=0.0;
-			}
-			else{
-				qz=q; qx=qy=0.0;
-			}
-			cf[iqdir][iq]=0.0;
-			for(imc=0;imc<NMC;imc++){
-				x=Rx*root2*randy.ran_gauss();
-				y=Ry*root2*randy.ran_gauss();
-				z=Rz*root2*randy.ran_gauss();
-				z=z+offset;
-				r=sqrt(x*x+y*y+z*z);
-				ctheta=(qx*x+qy*y+qz*z)/(q*r);
-				//c[iq]+=wf->GetPsiSquared(q,r,ctheta);
-				cf[iqdir][iq]+=wf->CalcPsiSquared(iq,r,ctheta);
-			}
-			cf[iqdir][iq]=cf[iqdir][iq]/double(NMC);
+  Nq=wf->GetNQMAX(); // Number of q values
+  cf.resize(3); // Do out/long/side
+  for(iqdir=0;iqdir<3;iqdir++){
+	cf[iqdir].resize(Nq);
+	for(iq=0;iq<Nq;iq++){
+		q=wf->GetQ(iq);
+		if(iqdir==0){
+			qx=q; qy=qz=0.0;
 		}
-		printf("--- finished %d / 3 directions ---\n",iqdir+1);
+		else if(iqdir==1){
+			qy=q; qx=qz=0.0;
+		}
+		else{
+			qz=q; qx=qy=0.0;
+		}
+		cf[iqdir][iq]=0.0;
+		for(imc=0;imc<NMC;imc++){
+			x=Rx*root2*randy.ran_gauss();
+			y=Ry*root2*randy.ran_gauss();
+			z=Rz*root2*randy.ran_gauss();
+			x=x+offset;
+			r=sqrt(x*x+y*y+z*z);
+			ctheta=(qx*x+qy*y+qz*z)/(q*r);
+			//cf[iqdir][iq]+=wf->GetPsiSquared(q,r,ctheta);
+			cf[iqdir][iq]+=wf->CalcPsiSquared(iq,r,ctheta);
+		}
+		cf[iqdir][iq]=cf[iqdir][iq]/double(NMC);
 	}
-	char filename[120];
-	sprintf(filename,"results/Rx%g_Ry%g_Rz%g_offset%g.dat",Rx,Ry,Rz,offset);
-	FILE *fptr=fopen(filename,"w");
-	printf(" q      Cout      Cside     Clong\n");
-	fprintf(fptr," q      Cout      Cside     Clong\n");
+	printf("--- finished %d / 3 directions ---\n",iqdir+1);
+}
+char filename[120];
+sprintf(filename,"results/Rx%g_Ry%g_Rz%g_offset%g.dat",Rx,Ry,Rz,offset);
+FILE *fptr=fopen(filename,"w");
+printf(" q      Cout      Cside     Clong\n");
+fprintf(fptr," q      Cout      Cside     Clong\n");
 	for(iq=0;iq<Nq;iq++){
 		q=wf->GetQ(iq);
 		printf("%5.1f %8.5f  %8.5f  %8.5f\n",q,cf[0][iq],cf[1][iq],cf[2][iq]);		
